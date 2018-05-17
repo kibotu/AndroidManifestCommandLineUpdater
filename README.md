66686b77022f0aab1207ed37a7e389f20f4fbf82
Jan Rabe <jan.rabe@exozet.com>
2018-05-17 20:41:16 +0200

    fixed paramter and added readme/license

---
+++ README.md
@@ -0,0 +1,36 @@
+# AndroidManifest.xml CommandLine Updater  [![Licence](https://img.shields.io/badge/licence-Apache%202-blue.svg)](https://raw.githubusercontent.com/kibotu/KalmanRx/master/LICENSE)
+
+# How to use
+
+### Change versionCode and versionNumber
+
+    java -jar build/libs/manifestupdater-1.0.0-all.jar -path=src/main/resources/AndroidManifest.xml -versionName=1.0.0 -versionCode=1024
+
+### Help
+
+    java -jar build/libs/manifestupdater-1.0.0-all.jar -help
+
+# How to build
+
+    gradle clean build
+
+## Contributors
+
+[Jan Rabe](jan.rabe@kibotu.net)
+
+### License
+<pre>
+Copyright 2018 Jan Rabe
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+</pre>
--- src/main/kotlin/net.kibotu.androidmanifestcommandlineupdater/Main.kt
+++ src/main/kotlin/net.kibotu.androidmanifestcommandlineupdater/Main.kt
@@ -10,30 +10,40 @@ import java.nio.charset.Charset
  */
 
 
-fun main(args: Array<String>?) {
+fun main(args: Array<String>) {
 
-    if (args?.find { it.contains("-help") } != null) {
+    if (args.find { it.contains("-help") } != null) {
         println("java -jar manifestupdater -versionCode=[d] -versionName=[w] -path=[s]")
         return
     }
 
-    val file = File(args?.find { it.contains("-path=") } ?: "src/main/resources/AndroidManifest.xml")
+    if (!args.any { it.contains("-path=") }) {
+        println("AndroidManifest.xml path is required: -path=")
+        return
+    }
+
+    val file = File(args.find { it.contains("-path=") }?.replace("-path=", "")
+            ?: "src/main/resources/AndroidManifest.xml")
+
     val manifest = file.inputStream().readTextAndClose()
-    val versionCode = args?.find { it.contains("-versionCode=") } ?: "1"
-    val versionName = args?.find { it.contains("-versionName=") } ?: "0.0.1"
+    val versionCode = args.find { it.contains("-versionCode=") }?.replace("-versionCode=", "")
+    val versionName = args.find { it.contains("-versionName=") }?.replace("-versionName=", "")
 
     val versionCodeMatcher = Regex("android:versionCode=\"(.*?)\"")
     val versionNameMatcher = Regex("android:versionName=\"(.*?)\"")
     val packageMatcher = Regex("package=\"(.*?)\"")
 
-    val currentVersionCode = versionNameMatcher.find(manifest)?.groupValues?.get(1)
-    val currentVersionName = versionCodeMatcher.find(manifest)?.groupValues?.get(1)
+    val currentVersionCode = versionCodeMatcher.find(manifest)?.groupValues?.get(1)
+    val currentVersionName = versionNameMatcher.find(manifest)?.groupValues?.get(1)
     val currentPackageName = packageMatcher.find(manifest)?.groupValues?.get(1)
 
-    var newManifest = manifest.replace(versionCodeMatcher, "android:versionCode=\"$versionCode\"")
-    newManifest = newManifest.replace(versionNameMatcher, "android:versionName=\"$versionName\"")
+    var newManifest = manifest
+    if (versionCode != null) newManifest = newManifest.replace(versionCodeMatcher, "android:versionCode=\"$versionCode\"")
+    if (versionName != null) newManifest = newManifest.replace(versionNameMatcher, "android:versionName=\"$versionName\"")
 
-    print("Updating versionCode: $currentVersionCode -> $versionCode and versionName: $currentVersionName -> $versionName for $currentPackageName \n${file.absolutePath}")
+    print("Updating versionCode: $currentVersionCode -> ${versionCode
+            ?: currentVersionCode} and versionName: $currentVersionName -> ${versionName
+            ?: currentVersionName} for $currentPackageName \n${file.absolutePath}")
 
     file.writeText(newManifest)
 }
